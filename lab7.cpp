f3dc43dd417e9cb2d235ac29d3c0183b68f4fcc8
ShyFly46 <61005033+ShyFly46@users.noreply.github.com>
2022-12-01 01:51:23 +0800

    Add files via upload

--- lab7.cpp
+++ lab7.cpp
@@ -5,72 +5,151 @@ struct List{
 	T v;
 	List *n;
 	
-	List(T v, List<T> *prev = nullptr){
+	List(T v, List<T> *prev){
 		this->v = v;
 		if(prev)
 			prev->n = this;
 		n = nullptr;
 	}
 	
-	List(){n = nullptr;}
-	
-	~List(){
-		if(n) delete n;
+	List(T v){
+		this->v = v;
+		n = nullptr;
 	}
+	
+	List(){n = nullptr;}
 };
 
 class ListMatrix{
 	List<List<int>*> *rows;
+	int width, hight;
 	
-	List<int>* fillrow(size_t w){
-		List<int> *ret = new List<int>;
-		List<int> *cur = ret;
+	void initsize(int h, int w){
+		if(h < 1 || w < 1) throw 1;
 		
-		int t;
-		std::cin >> t;
-		cur->v = t;
+		width = w;
+		hight = h;
 		
-		for(size_t i = 1; i < w; ++i){
-			std::cin >> t;
-			cur = new List<int>(t, cur);
+		List<List<int>*> *newMatrix, *rr;
+		List<int> *c, *cc;
+		
+		newMatrix = nullptr;
+		c = nullptr;
+		
+		//std::cout << h << " " << w << "\n";
+		
+		for(int i = 0; i < h; ++i) {
+			
+			for(int j = 0; j < w; ++j){
+				//std::cout << i << " " << j << "\n";
+				
+				cc = new List<int>(0);
+				cc->n = c;
+				c = cc;
+			}
+			
+			rr = new List<List<int>*>(c);
+			rr->n = newMatrix;
+			newMatrix = rr;
+			c = nullptr;
 		}
 		
-		return ret;
+		rows = newMatrix;
 	}
 	
   public:
-	void fill(size_t h, size_t w){
-		rows = new List<List<int>*>(fillrow(w));
+	ListMatrix(int h, int w){
+		initsize(h, w);
+	}
+	
+	void fill(){
+		List<List<int>*> *r = rows;
+		List<int> *c;
 		
-		List<List<int>*> *cur = rows;
-		for(size_t i = 1; i < h; ++i){
-			cur = new List<List<int>*>(fillrow(w), cur);
+		while(r){
+			c = r->v;
+			while(c){
+				int x;
+				std::cin >> x;
+				c->v = x;
+				c = c->n;
+			}
+			
+			r = r->n;
 		}
+	}
+	
+	void multiply(ListMatrix *a, ListMatrix *b){
+		if(a->hight != this->hight ||
+		   b->width != this->width ||
+		   a->width != b->hight) throw 1;
+		   
+		int depth = a->width;
+		
+		for(int i = 0; i < hight; ++i){
+			for(int j = 0; j < width; ++j){
+				int res = 0;
+				
+				for(int k = 0; k < depth; ++k){
+					res += a->at(i, k) * b->at(k, j);
+				}
+				
+				this->at(i, j) = res;
+			}
+		}
+	}
+	
+	int& at(int x, int y){
+		if(x < 0 || y < 0) throw 1;
+		
+		List<List<int>*> *r = rows;
+		List<int> *c;
+		
+		for(int i = 0; i < x; ++i){
+			r = r->n;
+		}
+		
+		c = r->v;
+		
+		for(int j = 0; j < y; ++j){
+			c = c->n;
+		}
+		
+		return c->v;
 	}
 	
 	void display(){
-		List<List<int>*> *ll = rows;
-		while(ll){
-			List<int> *l = ll->v;
-			while(l){
-				std::cout << l->v << " ";
-				l = l->n;
+		List<List<int>*> *r = rows;
+		List<int> *c;
+		
+		while(r){
+			c = r->v;
+			while(c){
+				std::cout << c->v << " ";
+				c = c->n;
 			}
 			std::cout << "\n";
-			
-			ll = ll->n;
+			r = r->n;
 		}
-	}
-	
-	int pop(size_t i, size_t j){
-		List<List<int>*>
-		for()
 	}
 };
 
 int main(){
-	ListMatrix *a = new ListMatrix;
+	using namespace std;
 	
-	a->fill(3, 4);
-	a->display();
+	int m, n, l;
+	cin >> m >> n >> l;
+	
+	ListMatrix *a = new ListMatrix(m, n);
+	ListMatrix *b = new ListMatrix(n, l);
+	ListMatrix *c = new ListMatrix(m, l);
+	
+	a->fill();
+	b->fill();
+	
+	c->multiply(a, b);
+	
+	c->display();
+	
+	//cout << a->at(1,1);
 }
